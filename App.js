65a40e08e4e3dfb0d029218db80300745142ed95
AUDIOAGARdotCOM <118103173+AUDIOAGARdotCOM@users.noreply.github.com>
2026-08-21 16:43:03 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,32 @@
 import React from 'react';
+import { SafeAreaView, Text, StyleSheet } from 'react-native';
 
 export default function App() {
-  return null;
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>FlashCARD</Text>
+      <Text style={styles.subtitle}>
+        Scansiona la tua prima carta fedeltà
+      </Text>
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    color: '#197A55'
+  },
+  subtitle: {
+    marginTop: 10,
+    fontSize: 18,
+    textAlign: 'center'
+  }
+});
